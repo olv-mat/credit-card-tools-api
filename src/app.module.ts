4023e671b9592af8c365e771a5c8722a5d02c459
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-02-16 09:24:18 -0300

    chore: generate tools module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ToolsModule } from './modules/tools/tools.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ToolsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
